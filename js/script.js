78c2809590bec7ed16b65b8bb7cbe935498957ef
mikzanco <mikyzancolo97@gmail.com>
2022-10-21 15:32:35 +0200

    add lista per procedere con ordine

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// creo lista delle cose da fare:
+/*
+
+COSA BISOGNA FARE:
+-Creare la griglia di gioco 
+-Creare i quadrati con id ed inserire i numeri
+-Genereo bombe id 
+-Creare evento click
+-Evento click che genera:
+
+ --FiNE GIOCO se bomba
+ --coloro casella se buona
+ --segno i tentativi in console
+ --quando tentativi = caselle buone FINITO GIOCO vinto.
+
+ 
+
+
+
+
+
+*/
+
+const grid = [100, 81, 49];
