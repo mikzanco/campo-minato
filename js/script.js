ea74dac5d539e92902844fdd73a2e998d5c23ff6
mikzanco <mikyzancolo97@gmail.com>
2022-10-21 16:58:59 +0200

    add campo da js sia griglia normale facile che le altre due griglir che cambiano a seconda della difficoltà selezionata. add azione sul pulsante start per creare il campo da gioco

--- js/script.js
+++ js/script.js
@@ -13,12 +13,70 @@ COSA BISOGNA FARE:
  --segno i tentativi in console
  --quando tentativi = caselle buone FINITO GIOCO vinto.
 
+ -FINE GIOCO 
+ -- segna il risultato
+ -- blocca griglia con un div absolute
+ --mi mostra tutte le bombe
  
+ -PLAY RESET
 
 
+-RESET 
 
+--cancello la vecchia griglia
+--camcello tutte le bombe
 
 
 */
 
-const grid = [100, 81, 49];
+
+const playBtn  = document.querySelector('#play');
+const levelSelect = document.querySelector('#level');
+const main = document.querySelector('.game-wrapped');
+
+const gridLevels = [100, 81, 49];
+const bombNumber = 16;
+
+// al click di start mi si genera la griglia con i quadrati con i numeri.
+
+playBtn.addEventListener('click', start);
+function start() {
+    const cellNumbers = gridLevels[levelSelect.value];
+
+    reset();
+
+// vado quindi a creare la griglia èpo attraverso la function
+    generatePlayGround(cellNumbers);
+}
+
+// funzione per la griglia
+function generatePlayGround(cellNumbers){
+
+    const grid = document.createElement('div');
+    grid.className = 'grid';
+
+    for(i = 1; i <= cellNumbers; i++){
+        const cell = generateCell(i, cellNumbers)
+        grid.append(cell);
+    }
+    main.append(grid);
+}
+
+function generateCell(cellId, cellNumbers){
+    const cell = document.createElement('div');
+    cell.className = 'cell';
+
+    cell.classList.add('square'+cellNumbers);
+
+    cell.cellId = cellId;
+    cell.innerHTML = `<span>${cellId}</span>`;
+    return cell;
+}
+
+
+
+
+// agg function reset per far si che al click si resetti tutto
+function reset(){
+    main.innerHTML = '';
+}
